507086fbab928453d0e7347c31b41226ac6d2f4e
Alfredo <alfredo@Carolinas-Laptop.local>
2024-05-19 11:12:15 -0400

    feat - Sign Up Page UI

--- app/(root)/(auth)/signup/page.tsx
+++ app/(root)/(auth)/signup/page.tsx
@@ -1,7 +1,77 @@
 import React from "react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const Page = () => {
-  return <div>Sign Up</div>;
+  return (
+    <Card className="mx-auto max-w-md">
+      <CardHeader>
+        <CardTitle className="text-2xl">Sign Up</CardTitle>
+        <CardDescription>
+          Enter your your data below to create an Account.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form className="grid gap-4">
+          <div className="grid gap-4 grid-cols-2">
+            <div className="grid gap-2">
+              <Label htmlFor="first-name">First Name</Label>
+              <Input id="first-name" type="text" placeholder="John" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="last-name">Last Name</Label>
+              <Input id="last-name" type="text" placeholder="Doe" required />
+            </div>
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="repeatPassword">Repeat Password</Label>
+            <Input id="repeatPassword" type="password" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="profile-photo">Profile Photo</Label>
+            <Input
+              id="profile-photo"
+              type="file"
+              accept="image/*"
+              className="file-input"
+            />
+          </div>
+          <Button type="submit" className="w-full">
+            Sign Up
+          </Button>
+        </form>
+        <div className="mt-4 text-center text-sm">
+          Already have an Account?{" "}
+          <Link href="/login" className="underline">
+            Login
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Page;
