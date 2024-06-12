62a5b460100b233a933739dda2dda45dfb924c7d
Alfredo - Maket AI <alfredo-maket@MacBook-Air.local>
2024-06-12 13:02:09 -0400

    Fixing cypress testing id

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    viewportWidth: 1280,
-    viewportHeight: 800,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        viewportWidth: 1280,
+        viewportHeight: 800,
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        projectId: 'dbsy94',
     },
-  },
 });
